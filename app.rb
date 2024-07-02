45841a182c6fd19b7f32f0e9524ebb67017b7415
Stephen Steiner <46692873+stephensteiner4@users.noreply.github.com>
2024-07-02 23:57:32 +0000

    start omnicalc application and create results page

--- app.rb
+++ app.rb
@@ -7,3 +7,37 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+### Squared
+get("/square/new") do
+
+  erb(:square_home)
+
+end
+
+get("/square/results") do
+  @number = params.fetch("number").to_i
+
+  @sq_number = (@number ** 2).to_f
+
+  @page = "Square"
+
+  params["Square"] = @sq_number
+
+  erb(:results)
+end
+
+### Square root
+get("/square_root/new") do
+
+  erb(:square_root_home)
+
+end
+
+get("/square_root/results") do
+  @number = params.fetch("number").to_i
+
+  @sqrt_number = (@number ** (1/2)).to_f
+
+  erb(:square_root)
+end
